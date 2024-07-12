b2ced746d200b3862ff6566bd03341deb57e56ac
peter-oka <240889648@qq.com>
2024-07-13 00:06:03 +0800

    test01

---
+++ src/main/java/org/example/testgit/controller/TestController.java
@@ -0,0 +1,8 @@
+package org.example.testgit.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    public static final String WORD_TEST = "word_test";
+}
